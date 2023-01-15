586124a830f32897454dad1c47ab692ce3d1c321
ahmedAlaa24494 <ahmedalaa24494@gmail.com>
2023-01-15 12:36:16 +0200

    troubleshoot longitude boundries in the cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,7 +33,8 @@ def go(args):
     # Convert last_review to datetime
     logger.info("Convert last_review date from str to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
-
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # #Fill NA values
     # df['last_review'] = df['last_review'].fillna('')
     # df['name'] = df['name'].fillna('')
